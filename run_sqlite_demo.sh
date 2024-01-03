caedf240b1274866b9907493cf0bf93401627c3a
Brian Kroth <bpkroth@users.noreply.github.com>
2024-01-03 15:17:16 -0600

    Various fixups (#42)
    - Fixup path conversion fixups for Docker mounts
    - Add a script to make running the default command easier
    - Sync settings for VSCode from mlos-autotuning-template repo

---
+++ run_sqlite_demo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+scriptdir=$(dirname "$(readlink -f "$0")")
+cd "$scriptdir/"
+
+set -x
+conda run -n mlos \
+    mlos_bench --config config/cli/local-sqlite-opt.jsonc \
+        --globals config/experiments/sqlite-sync-journal-pagesize-caching-experiment.jsonc \
+        --max-iterations 100 \
+#        --log-level DEBUG
