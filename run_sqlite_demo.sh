aabc6df548cfbe5f7884087269feac3204bdcd44
Brian Kroth <bpkroth@microsoft.com>
2024-01-10 21:56:52 +0000

    install some additional dependencies for our scripts

--- run_sqlite_demo.sh
+++ run_sqlite_demo.sh
@@ -6,6 +6,7 @@ scriptdir=$(dirname "$(readlink -f "$0")")
 cd "$scriptdir/"
 
 set -x
+pip install -U requirements.txt
 conda run -n mlos \
     mlos_bench --config config/cli/local-sqlite-opt.jsonc \
         --globals config/experiments/sqlite-sync-journal-pagesize-caching-experiment.jsonc \
