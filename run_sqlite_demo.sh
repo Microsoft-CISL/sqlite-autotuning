9973c44d72b73db9f9c613ae9fa31f7f1f9ce213
Brian Kroth <bpkroth@microsoft.com>
2024-01-18 22:01:48 +0000

    repeat trials by default

--- run_sqlite_demo.sh
+++ run_sqlite_demo.sh
@@ -10,5 +10,6 @@ pip install -U -r requirements.txt
 conda run -n mlos \
     mlos_bench --config config/cli/local-sqlite-opt.jsonc \
         --globals config/experiments/sqlite-sync-journal-pagesize-caching-experiment.jsonc \
+        --trial-config-repeat-count 3 \
         --max-iterations 100 \
 #        --log-level DEBUG
