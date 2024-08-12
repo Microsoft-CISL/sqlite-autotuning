a4b01e2685cedcca23358660a08d1a3f371084fe
Brian Kroth <bpkroth@microsoft.com>
2024-08-12 22:59:21 +0000

    wip stubs

---
+++ .devcontainer/build/build-devcontainer-cli.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+##
+## Copyright (c) Microsoft Corporation.
+## Licensed under the MIT License.
+##
+
+set -x
+
+set -eu
+scriptdir=$(dirname "$(readlink -f "$0")")
+cd "$scriptdir/"
+
+# Build the helper container that has the devcontainer CLI for building the devcontainer.
+
+if [ ! -w /var/run/docker.sock ]; then
+    echo "ERROR: $USER does not have write access to /var/run/docker.sock. Please add $USER to the docker group." >&2
+    exit 1
+fi
+DOCKER_GID=$(stat -c'%g' /var/run/docker.sock)
+# Make this work inside a devcontainer as well.
+if [ -w /var/run/docker-host.sock ]; then
+    DOCKER_GID=$(stat -c'%g' /var/run/docker-host.sock)
+fi
+
+export DOCKER_BUILDKIT=${DOCKER_BUILDKIT:-1}
+devcontainer_cli_build_args=''
+if docker buildx version 2>/dev/null; then
+    devcontainer_cli_build_args+=' --progress=plain'
+else
+    echo 'NOTE: docker buildkit unavailable.' >&2
+fi
+
+if [ "${NO_CACHE:-}" == 'true' ]; then
+    devcontainer_cli_build_args+=' --no-cache --pull'
+else
+    cacheFrom='mloscore.azurecr.io/devcontainer-cli:latest'
+    tmpdir=$(mktemp -d)
+    devcontainer_cli_build_args+=" --cache-from $cacheFrom"
+    docker --config="$tmpdir" pull "$cacheFrom" || true
+    rmdir "$tmpdir"
+fi
+
+docker build -t devcontainer-cli:latest -t cspell:latest -t markdown-link-check:latest \
+    $devcontainer_cli_build_args \
+    --build-arg BUILDKIT_INLINE_CACHE=1 \
+    --build-arg NODE_UID=$(id -u) \
+    --build-arg NODE_GID=$(id -g) \
+    --build-arg DOCKER_GID=$DOCKER_GID \
+    --build-arg http_proxy=${http_proxy:-} \
+    --build-arg https_proxy=${https_proxy:-} \
+    --build-arg no_proxy=${no_proxy:-} \
+    -f Dockerfile .
+if [ "${CONTAINER_REGISTRY:-}" != '' ]; then
+    docker tag devcontainer-cli:latest "$CONTAINER_REGISTRY/devcontainer-cli:latest"
+fi
---
+++ .devcontainer/build/build-devcontainer.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+##
+## Copyright (c) Microsoft Corporation.
+## Licensed under the MIT License.
+##
+
+set -x
+
+set -eu
+scriptdir=$(dirname "$(readlink -f "$0")")
+cd "$scriptdir/"
+
+# Build the helper container that has the devcontainer CLI for building the devcontainer.
+NO_CACHE=${NO_CACHE:-} ./build-devcontainer-cli.sh
+
+DOCKER_GID=$(stat -c'%g' /var/run/docker.sock)
+# Make this work inside a devcontainer as well.
+if [ -w /var/run/docker-host.sock ]; then
+    DOCKER_GID=$(stat -c'%g' /var/run/docker-host.sock)
+fi
+
+# Build the devcontainer image.
+rootdir=$(readlink -f "$scriptdir/../..")
+
+# Run the initialize command on the host first.
+# Note: command should already pull the cached image if possible.
+pwd
+devcontainer_json=$(cat "$rootdir/.devcontainer/devcontainer.json" | sed -e 's|//.*||' -e 's|/\*|\n&|g;s|*/|&\n|g' | sed -e '/\/\*/,/*\//d')
+initializeCommand=$(echo "$devcontainer_json" | docker run -i --rm devcontainer-cli jq -e -r '.initializeCommand[]')
+if [ -z "$initializeCommand" ]; then
+    echo "No initializeCommand found in devcontainer.json" >&2
+    exit 1
+else
+    eval "pushd "$rootdir/"; $initializeCommand; popd"
+fi
+
+devcontainer_build_args=''
+if [ "${NO_CACHE:-}" == 'true' ]; then
+    base_image=$(grep '^FROM ' "$rootdir/.devcontainer/Dockerfile" | sed -e 's/^FROM //' -e 's/ AS .*//' | head -n1)
+    docker pull "$base_image" || true
+    devcontainer_build_args='--no-cache'
+else
+    cache_from='mloscore.azurecr.io/mlos-devcontainer:latest'
+    devcontainer_build_args="--cache-from $cache_from --cache-from mlos-devcontainer:latest"
+    tmpdir=$(mktemp -d)
+    docker --config="$tmpdir" pull "$cache_from" || true
+    rmdir "$tmpdir"
+fi
+
+# Make this work inside a devcontainer as well.
+if [ -n "${LOCAL_WORKSPACE_FOLDER:-}" ]; then
+    rootdir="$LOCAL_WORKSPACE_FOLDER"
+fi
+
+docker run -i --rm \
+    --user $(id -u):$DOCKER_GID \
+    -v "$rootdir":/src \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --env DOCKER_BUILDKIT=${DOCKER_BUILDKIT:-1} \
+    --env BUILDKIT_INLINE_CACHE=1 \
+    --env http_proxy=${http_proxy:-} \
+    --env https_proxy=${https_proxy:-} \
+    --env no_proxy=${no_proxy:-} \
+    devcontainer-cli \
+    devcontainer build --workspace-folder /src \
+        $devcontainer_build_args \
+        --image-name mlos-devcontainer:latest
+if [ "${CONTAINER_REGISTRY:-}" != '' ]; then
+    docker tag mlos-devcontainer:latest "$CONTAINER_REGISTRY/mlos-devcontainer:latest"
+fi
