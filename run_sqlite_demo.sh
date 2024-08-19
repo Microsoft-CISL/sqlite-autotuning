572cb9ac8ce47ff8eec3d9ef0cfa32f05ab8ed9d
Brian Kroth <bpkroth@microsoft.com>
2024-08-19 22:21:15 +0000

    tweaks

--- run_sqlite_demo.sh
+++ run_sqlite_demo.sh
@@ -15,5 +15,5 @@ conda run -n mlos \
     mlos_bench --config config/cli/local-sqlite-opt.jsonc \
         --globals config/experiments/sqlite-sync-journal-pagesize-caching-experiment.jsonc \
         --trial-config-repeat-count $TRIAL_CONFIG_REPEAT_CONFIG \
-        --max-suggestions $MAX_SUGGESTIONS \
+        --max_suggestions $MAX_SUGGESTIONS \
         --log-level $LOG_LEVEL
