5ab35260682f57e4837a66f33f3f5c560b454ce9
Brian Kroth <bpkroth@microsoft.com>
2024-08-21 21:55:35 +0000

    more tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,3 +11,8 @@ scriptdir=$(dirname "$(readlink -f "$0")")
 cd "$scriptdir/.."
 
 TRIAL_CONFIG_REPEAT_COUNT=2 MAX_SUGGESTIONS=2 LOG_LEVEL=INFO ./run_sqlite_demo.sh
+
+# TODO: Validate that the number of trials increased as expected.
+
+# Run the jupyter notebooks.
+find . -maxdepth 2 -name '*.ipynb' -print0 | xargs -t -0 -P0 -n1 conda run -n mlos jupyter execute
